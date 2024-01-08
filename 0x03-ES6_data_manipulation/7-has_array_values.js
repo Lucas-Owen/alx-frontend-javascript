67156bc85fcdf36560b000889e871c43b65989aa
Lucas Sangura <sanguraowens@gmail.com>
2024-01-08 20:36:30 +0300

    Complete basic tasks

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,3 @@
+export default function hasValuesFromArray(set, array) {
+  return array.map(val => set.has(val)).reduce((prev, curr) => prev && curr, true)
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,5 @@
+export default function cleanSet(set, startString) {
+  return Array.from(set).filter(string => startString && string.startsWith(startString))
+    .map(string => string.replace(startString, ""))
+    .join("-");
+}
